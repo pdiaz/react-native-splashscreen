173491e668fb939bb08fb943ff67eb9540553a4c
Pedro Diaz <p.diaz@wemersive.com>
2016-03-15 13:49:45 +0100

    Increased the delay on the cross fade with the splash on iOS

--- ios/RCTSplashScreen/RCTSplashScreen.m
+++ ios/RCTSplashScreen/RCTSplashScreen.m
@@ -36,7 +36,7 @@ RCT_EXPORT_METHOD(hide)
     if (!rootView) {
         return;
     }
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)),
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)),
                    dispatch_get_main_queue(),
                    ^{
                        [UIView animateWithDuration:0.4
